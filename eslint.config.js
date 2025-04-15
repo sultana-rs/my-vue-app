a404fb8fec64dfbb1421296448f3b948cbbf17f8
sultana-rs <r.sultana@lila2.de>
2025-04-15 16:20:17 +0200

    less

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
